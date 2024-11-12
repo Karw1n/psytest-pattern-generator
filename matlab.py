6db0ed4a1a3a65902f3c6c7f5d7274229228214d
Karw1n <akbbalfour@gmail.com>
2024-11-12 10:43:29 +0000

    Added a border boxe to replace axis on image.

--- matlab.py
+++ matlab.py
@@ -4,8 +4,15 @@ import matplotlib.patches as patches
 fix, ax = plt.subplots()
 ax.set_aspect('equal')
 
-ax.set_xlim(-20, 20)
-ax.set_ylim(-20, 20)
+width,  height = 20, 20
+
+ax.set_xlim(0, width)
+ax.set_ylim(0, height)
+center = (width/2, height/2)
+
+# Border Box
+border = patches.Rectangle((0,0), width=width, height=height, edgecolor='black', fill='light_blue')
+ax.add_patch(border)
 
 
 # Rectangle with no fill
@@ -15,6 +22,6 @@ ax.add_patch(rect)
 circle = patches.Circle((2, 5), radius=2.5, edgecolor='red', fill=False)
 ax.add_patch(circle)
 
-plt.axis('on')
+plt.axis('off')
 
 plt.show()
