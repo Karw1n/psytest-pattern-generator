1ae16ac3af18f7092dde42fa4b5583fa2a9745e1
Karw1n <akbbalfour@gmail.com>
2024-11-13 12:13:30 +0000

    Added for loop that moves circles in a clockwise motion on each plot (minus the last plot)

--- test.py
+++ test.py
@@ -3,31 +3,32 @@ import matplotlib.patches as patches
 
 
 # 
-fig, axs = plt.subplots(1, 4, figsize=(10, 10))
+fig, axs = plt.subplots(1, 5, figsize=(10, 10))
 
-width, height = 10, 10
+width, height = 20, 20
+bottom_left_corner_coord = (-width/2, -height/2) 
+corner_coords = [(-width/2, height/2), (width/2, height/2), (width/2, -height/2), (-width/2, -height/2)]
+
 for ax in axs.flat:
     ax.set_aspect('equal')
-    ax.set_xlim(0, width)
-    ax.set_ylim(0, height)
+    
+    ax.set_xlim(-width/2, width/2)
+    ax.set_ylim(-height/2, height/2)
     
     # Set the image border
-    border = patches.Rectangle((0,0), width=width, height=height, edgecolor='black', fill='light_blue')
+    border = patches.Rectangle((corner_coords[3]), width=width, height=height, edgecolor='black', fill='light_blue')
     ax.add_patch(border)
-    ax.axis('off')
+    #ax.axis('off')
 
     
     
 # Create the first image of the pattern
-for i in range (3):
-    circle = patches.Circle((2.5, 7.5),  radius=1, color='red')
+for i in range (4):
+    circle = patches.Circle((corner_coords[i % 4]),  radius=2, color='red')
     (axs.flat[i]).add_patch(circle)
 
 
 # Incremently apply the rule
-
-
-
 
 
 plt.tight_layout()
